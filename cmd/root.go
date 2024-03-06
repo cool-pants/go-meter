7329f61fb07222dc09f8d872adf7184fa4d59594
cool-pants <muban.sg@gmail.com>
2024-03-06 11:57:38 +0530

    📝: update help text

--- cmd/root.go
+++ cmd/root.go
@@ -9,9 +9,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "gogeta",
 	Short: "gogeta is a distributed load testing framework",
-	Long: `A lightweight and fast load testing framework
-			inspired by https://github.com/tsenart/vegeta.
-			Supports scripting using yaml/json`,
+	Long:  `A lightweight and fast load testing framework inspired by https://github.com/tsenart/vegeta. Supports scripting using yaml/json`,
 }
 
 func Execute() {
