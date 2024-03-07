10790f1a9b020b70e02929d400a08270fff43d53
cool-pants <muban.sg@gmail.com>
2024-03-07 17:41:16 +0530

    ✨: add file handler

---
+++ cmd/file.go
@@ -0,0 +1,17 @@
+package commands
+
+import "os"
+
+func file(name string, create bool) (*os.File, error) {
+	switch name {
+	case "stdin":
+		return os.Stdin, nil
+	case "stdout":
+		return os.Stdout, nil
+	default:
+		if create {
+			return os.Create(name)
+		}
+		return os.Open(name)
+	}
+}
